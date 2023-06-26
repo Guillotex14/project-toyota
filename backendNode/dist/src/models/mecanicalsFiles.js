c54e6f963d42a7bcfee98e94a783ad239d088a25
jefersonmujica <jefersonmujica@gmail.com>
2023-06-25 23:50:34 -0400

    subiendo todo actualizado

--- backendNode/dist/src/models/mecanicalsFiles.js
+++ backendNode/dist/src/models/mecanicalsFiles.js
@@ -5,7 +5,10 @@ const medicalFile = new mongoose_1.Schema({
     disease: String,
     allergy: String,
     condiction: String,
-    additional: String
+    additional: String,
+    id_vehicle: {
+        type: mongoose_1.Schema.Types.ObjectId,
+    }
 });
 exports.default = (0, mongoose_1.model)("medicalFile", medicalFile);
 //# sourceMappingURL=mecanicalsFiles.js.map
