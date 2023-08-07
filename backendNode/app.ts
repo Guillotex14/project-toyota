b2830fdaef3975e2d06fcd3615fef1f84c2fe087
jefersonmujica <jefersonmujica@gmail.com>
2023-08-07 15:42:02 -0400

    aumentando urlencode y json body parse

--- backendNode/app.ts
+++ backendNode/app.ts
@@ -25,9 +25,9 @@ export class App {
     middlewares() {
         this.app.use(Express.json());
         this.app.use(cors())
-        this.app.use(Express.json({limit: '50mb'}));
-        this.app.use(Express.urlencoded({limit: '50mb'}));
-        this.app.use(Express.urlencoded({ extended: true }));
+        this.app.use(Express.json({limit: '512mb'}));
+        this.app.use(Express.urlencoded({limit: '512mb', extended: true}));
+        // this.app.use(Express.urlencoded({  }));
     }
 
     routes() {
