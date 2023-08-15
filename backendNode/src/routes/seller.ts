60e89d1b751c055daeff2db92f79bab73b8a469d
eduardo.castillo <eduardo.castillo@thepapayagroup.com>
2023-08-15 00:21:03 -0400

    yeah

--- backendNode/src/routes/seller.ts
+++ backendNode/src/routes/seller.ts
@@ -1448,6 +1448,8 @@ sellerRouter.get("/exportExcell", async (req: Request, res: Response) => {
   let to_at = `${now.getFullYear()}-12-31`;
 
   let mongQuery: any = {
+    sold: true, // Campo de búsqueda adicional
+    dispatched: true,
     date_sell: {
       $gte: from_at,
       $lte: to_at,
@@ -1872,6 +1874,8 @@ sellerRouter.get("/listVehiclesSell", async (req: Request, res: Response) => {
   let to_at = `${now.getFullYear()}-12-31`;
 
   let mongQuery: any = {
+    sold: true, // Campo de búsqueda adicional
+    dispatched: true,
     date_sell: {
       $gte: from_at,
       $lte: to_at,
