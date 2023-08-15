bd202c71dcb895359e0587e493c5f8f33e5a9322
jefersonmujica <jefersonmujica@gmail.com>
2023-08-15 01:55:26 -0400

    cambios

--- shopCarApp/src/app/graphics-admin/graphics-admin.page.ts
+++ shopCarApp/src/app/graphics-admin/graphics-admin.page.ts
@@ -181,7 +181,7 @@ export class GraphicsAdminPage implements AfterViewInit {
   }
 
   public getCarList(){
-    console.log(this.id_user)
+    
     let data = {
       dateTo: this.dateTo,
       dateFrom: this.dateFrom,
@@ -194,35 +194,6 @@ export class GraphicsAdminPage implements AfterViewInit {
     this.sellerSrv.getListCars(data).subscribe((res:any)=>{
       console.log(res)
       if (res.status) {
-        if (res.data.grupocard.length > 0) {
-          for (let i = 0; i < res.data.grupocard.length; i++) {
-            if (res.data.grupocard[i].vehiclesWithImages.length > 0) {
-              for (let j = 0; j < res.data.grupocard[i].vehiclesWithImages.length; j++) {
-                if (res.data.grupocard[i].vehicles.length > 0){
-                  for (let k = 0; k < res.data.grupocard[i].vehicles.length; k++) {
-                    if (res.data.grupocard[i].vehiclesWithImages[j].id_vehicle == res.data.grupocard[i].vehicles[k]._id) {
-                      res.data.grupocard[i].vehicles[k].image = res.data.grupocard[i].vehiclesWithImages[j].img;
-                    }else{
-                      res.data.grupocard[i].vehicles[k].image ="";
-                    }
-                  }
-                }else{
-                  if (res.data.grupocard[i].vehicles.length > 0){
-                    for (let k = 0; k < res.data.grupocard[i].vehicles.length; k++) {
-                      res.data.grupocard[i].vehicles[k].image ="";
-                    }
-                  }
-                }
-              }
-            }else{
-              if (res.data.grupocard[i].vehicles.length > 0){
-                for (let k = 0; k < res.data.grupocard[i].vehicles.length; k++) {
-                  res.data.grupocard[i].vehicles[k].image ="";
-                }
-              }
-            }
-          }
-        }
 
         this.arrayListCars = res.data.grupocard;
       }else{
