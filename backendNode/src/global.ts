f5c3be7afe38363e88464ae10006b6047def73e1
eduanc2 <eduanc2@gmail.com>
2023-09-26 23:20:06 -0400

    yeah

--- backendNode/src/global.ts
+++ backendNode/src/global.ts
@@ -1,2 +1,3 @@
-export const urlBase = "https://project-toyota-backend.up.railway.app/";
-// export const urlBase = "http://localhost:3000/";
+// export const urlBase = "https://project-toyota-backend.up.railway.app/";
+
+export const urlBase = "http://localhost:3000/";
