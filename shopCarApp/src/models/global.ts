5bc0a2d37666c26c99f467099bc5ee8823e658b3
jefersonmujica <jefersonmujica@gmail.com>
2024-01-29 19:05:07 -0400

    url base

--- shopCarApp/src/models/global.ts
+++ shopCarApp/src/models/global.ts
@@ -1,6 +1,6 @@
-// export const urlBase = "https://project-toyota-backend.up.railway.app/";
+export const urlBase = "https://project-toyota-backend.up.railway.app/";
 
-export const urlBase = "http://localhost:3000/";
+// export const urlBase = "http://localhost:3000/";
 
 export const urlImgUser = urlBase + "public/images/users/";
 
